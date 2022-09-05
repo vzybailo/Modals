887ac7ad4fe69195897cb8f457182ec5fb462f82
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2022-09-05 18:26:27 +0300

    first-day

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+          <modal 
+            :title="title"
+            v-if="modalFirst"
+            @close="modalFirst = !modalFirst">          
+          </modal>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import modal from '@/components/Modal.vue'
+
+export default {
+  components: { modal },
+  data () {
+    return {
+      modalFirst: false,
+      title: 'First modal window'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
